46c7f292c4467741b0dbf7287af021559c469491
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2020-03-02 20:36:16 -0800

    fixed modal views

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,7 +7,7 @@ function Navbar() {
                 <div className="nav-wrapper">
                     <img href="/" className="brand-logo logo-img valign-wrapper" alt="logo" src="https://project-3-arcade-game.s3-us-west-2.amazonaws.com/longwaylog.png" />
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="/">Play</a></li>
+                        {/* <li><a href="/">Play</a></li> */}
                         <li><a href="/">About</a></li>
                         <li><a href="/">Credits</a></li>
                     </ul>
