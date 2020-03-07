f94491aee453fa0e7d7bfc0dfc7cbc26bbe0ef5a
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2020-03-07 10:21:26 -0800

    saving current changes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import "./pages/Home/style.css"
 import Navbar from './components/Navbar';
 import Game from './pages/Game';
 import Home from './pages/Home'
-<<<<<<< HEAD
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
-import Characters from './pages/Characters'
-=======
-import CharSelection from './pages/CharSelection';
->>>>>>> 9e6e47d235ad492870feec9ba60533b9f043b50a
+import CharSelection from './pages/CharSelection'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -20,12 +16,8 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
-<<<<<<< HEAD
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/characters" component={Characters} />
-=======
->>>>>>> 9e6e47d235ad492870feec9ba60533b9f043b50a
           <Route exact path="/game" component={Game} />
           <Route exact path="/characters" component={CharSelection} />
         </Switch>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,17 +7,11 @@ function Navbar() {
                 <div className="nav-wrapper">
                     <img href="/" className="brand-logo logo-img valign-wrapper" alt="logo" src="https://project-3-arcade-game.s3-us-west-2.amazonaws.com/longwaylog.png" />
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-<<<<<<< HEAD
-                        <li><a href="/">Play</a></li>
-                        <li><a href="/">About</a></li>
-                        <li><a href="/">Credits</a></li>
-=======
                         <li><a href="/">Home</a></li>
                         <li><a href="/game">Play</a></li>
                         <li><a href="/about">About</a></li>
                         <li><a href="/credits">Credits</a></li>
                         <li><a href="/characters">Characters</a></li>
->>>>>>> 9e6e47d235ad492870feec9ba60533b9f043b50a
                     </ul>
                 </div>
             </div>
