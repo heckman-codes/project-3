afaec9d157d863b01d1c02adb10cec15170497fe
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2020-03-14 13:16:33 -0700

    added routes for login

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const db = require("../models");
 // Defining methods for the postsController
 module.exports = {
     findById: function (req, res) {
-        db.User.findById(req.params._id)
+        db.User.findById(req.params.id)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
--- routes/api/login.js
+++ routes/api/login.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userController = require('../../controllers/userController');
 
 router
-    .route('/login/:id')
+    .route('/login')
     .get(userController.findById)
 
 module.exports = router 
