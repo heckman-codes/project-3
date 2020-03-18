1f9dcc4642a573172f04872adefee1eba984b05c
heckman-codes <liamheckmancode@gmail.com>
2020-03-18 16:11:34 -0700

    pulling caitlans stuff

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default {
 
     // Gets the Users with the given id
-<<<<<<< HEAD
     getLogin: function (userLoginData) {
         console.log(userLoginData);
         return axios.get("/api/login/", userLoginData);
@@ -15,11 +14,6 @@ export default {
     // Saves a User to the database
     saveLogin: function (loginData) {
         return axios.post("/api/login", loginData);
-=======
-    getLogin: function (id) {
-        console.log(id);
-        return axios.get("/api/login/" + id);
->>>>>>> 9aaa3097e6366d6489769b53696bf8a35a96ac0b
     },
     // Saves a User to the database
     addUser: function (signupData) {
