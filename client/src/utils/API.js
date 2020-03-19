4e5940021d6f3eb8f53c547c61b7ad33300fde64
Kevin-eh <54459190+Kevin-eh@users.noreply.github.com>
2020-03-18 17:44:38 -0700

    code error fix

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default {
   // Gets the Users with the given id
   getLogin: function(userLoginData) {
-    gi;
     console.log(userLoginData);
     return axios.get("/api/login/", userLoginData);
   },
